a75f66e78a55f1f88a95dc65cb049c38b9e8dbf3
Maciej Hirsz <maciej.hirsz@gmail.com>
2016-07-10 23:04:17 +0200

    Working on performance, broke tests with e notation

--- src/write.rs
+++ src/write.rs
@@ -8,131 +8,150 @@ const DEC_DIGITS_LUT: &'static[u8] =
       6061626364656667686970717273747576777879\
       8081828384858687888990919293949596979899";
 
+#[inline(always)]
+unsafe fn write_num(n: &mut isize, curr: &mut isize, buf_ptr: *mut u8, lut_ptr: *const u8) {
+    // eagerly decode 4 digits at a time
+    while *n >= 10000 {
+        let rem = *n % 10000;
+        *n /= 10000;
 
-const ZEROES: &'static[u8] = b"00000000000000000000000000000000";
+        let d1 = (rem / 100) << 1;
+        let d2 = (rem % 100) << 1;
+        *curr -= 4;
+        ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(*curr), 2);
+        ptr::copy_nonoverlapping(lut_ptr.offset(d2), buf_ptr.offset(*curr + 2), 2);
+    }
+
+    // decode 2 more digits
+    if *n >= 100 {
+        let d1 = (*n % 100) << 1;
+        *n /= 100;
+        *curr -= 2;
+        ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(*curr), 2);
+    }
+
+    // decode last 1 or 2 digits
+    if *n < 10 {
+        *curr -= 1;
+        *buf_ptr.offset(*curr) = (*n as u8) + b'0';
+    } else {
+        let d1 = *n << 1;
+        *curr -= 2;
+        ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(*curr), 2);
+    }
+}
 
 impl Dec64 {
     pub fn write<W: io::Write>(self, wr: &mut W) -> io::Result<()> {
-        let coefficient = self.coefficient();
-        let exponent    = self.exponent();
+        let mut n = self.coefficient() as isize;
+        let e = self.exponent() as i16;
 
-        if coefficient == 0 {
+        if n == 0 {
             return wr.write_all(b"0");
-        } else if exponent == -128 {
+        } else if e == -128 {
             return wr.write_all(b"nan");
         }
 
-        let mut n = if coefficient >= 0 {
-            coefficient
-        } else {
+        if n < 0 {
             try!(wr.write_all(b"-"));
             // convert the negative num to positive by summing 1 to it's 2 complement
-            (!coefficient).wrapping_add(1)
-        };
-        let mut buf: [u8; 30] = unsafe { mem::uninitialized() };
+            n = -n;
+        }
+
+        let mut buf: [u8; 24] = unsafe { mem::uninitialized() };
         let mut curr = buf.len() as isize;
         let buf_ptr = buf.as_mut_ptr();
         let lut_ptr = DEC_DIGITS_LUT.as_ptr();
 
         unsafe {
-            // eagerly decode 4 characters at a time
-            while n >= 10000 {
-                let rem = (n % 10000) as isize;
-                n /= 10000;
+            if e < 0 {
+                let mut e = -e as u16;
 
-                let d1 = (rem / 100) << 1;
-                let d2 = (rem % 100) << 1;
-                curr -= 4;
-                ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
-                ptr::copy_nonoverlapping(lut_ptr.offset(d2), buf_ptr.offset(curr + 2), 2);
-            }
+                // Decimal number with a fraction that's fully printable
+                if e < 18 {
+                    // eagerly decode 4 digits at a time
+                    for _ in 0 .. e >> 2 {
+                        let rem = n % 10000;
+                        n /= 10000;
 
-            // if we reach here numbers are <= 9999, so at most 4 chars long
-            let mut n = n as isize; // possibly reduce 64bit math
-
-            // decode 2 more chars, if > 2 chars
-            if n >= 100 {
-                let d1 = (n % 100) << 1;
-                n /= 100;
-                curr -= 2;
-                ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
-            }
-
-            // decode last 1 or 2 chars
-            if n < 10 {
-                curr -= 1;
-                *buf_ptr.offset(curr) = (n as u8) + 48;
-            } else {
-                let d1 = n << 1;
-                curr -= 2;
-                ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
-            }
-
-            if exponent < 0 {
-                let mut e = (!exponent).wrapping_add(1) as usize;
-
-                if e > 30 {
-                    // panic!("CURR {}", curr);
-                    ptr::copy(buf_ptr.offset(1), buf_ptr, curr as usize);
-                    *buf_ptr.offset(curr) = b'.';
-                    curr -= 1;
-                    e -= buf.len() - (curr as usize) - 2;
-
-                    try!(wr.write_all(
-                        slice::from_raw_parts(buf_ptr.offset(curr), buf.len() - curr as usize)
-                    ));
-                    try!(wr.write_all(b"e-"));
-
-                    let e = e as isize;
-
-                    return if e > 100 {
-                        // 127 is highest exponent, so the first digit can be only `1`
-                        try!(wr.write_all(b"1"));
-                        wr.write_all(
-                            slice::from_raw_parts(lut_ptr.offset((e % 100) << 1), 2)
-                        )
-                    } else if e > 10 {
-                        wr.write_all(
-                            slice::from_raw_parts(lut_ptr.offset(e << 1), 2)
-                        )
-                    } else {
-                        wr.write_all(&[(e as u8) + b'0'])
-                    };
-                } else {
-                    let written = (buf.len() - curr as usize) - 1;
-
-                    if e > written {
-                        let zeroprefill = e - written;
-                        curr -= zeroprefill as isize;
-                        ptr::copy_nonoverlapping(ZEROES.as_ptr(), buf_ptr.offset(curr), zeroprefill);
+                        let d1 = (rem / 100) << 1;
+                        let d2 = (rem % 100) << 1;
+                        curr -= 4;
+                        ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
+                        ptr::copy_nonoverlapping(lut_ptr.offset(d2), buf_ptr.offset(curr + 2), 2);
                     }
 
-                    ptr::copy(buf_ptr.offset(1), buf_ptr, buf.len() - (e as usize));
-                    *buf_ptr.offset((buf.len() as isize) + (exponent as isize) - 1) = b'.';
+                    e &= 3;
+
+                    // write the remaining 3, 2 or 1 digits
+                    if e & 2 == 2 {
+                        let d1 = (n % 100) << 1;
+                        n /= 100;
+                        curr -= 2;
+                        ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
+                    }
+
+                    if e & 1 == 1 {
+                        curr -= 1;
+                        *buf_ptr.offset(curr) = ((n % 10) as u8) + b'0';
+                        n /= 10;
+                    }
+
                     curr -= 1;
-                }
-            } else if exponent > 0 {
-                try!(wr.write_all(
-                    slice::from_raw_parts(buf_ptr.offset(curr), buf.len() - curr as usize)
-                ));
-                try!(wr.write_all(b"e"));
+                    *buf_ptr.offset(curr) = b'.';
 
-                let e = exponent as isize;
+                    write_num(&mut n, &mut curr, buf_ptr, lut_ptr);
 
-                return if e > 100 {
-                    // 127 is highest exponent, so the first digit can be only `1`
-                    try!(wr.write_all(b"1"));
-                    wr.write_all(
-                        slice::from_raw_parts(lut_ptr.offset((e % 100) << 1), 2)
-                    )
-                } else if e > 10 {
-                    wr.write_all(
-                        slice::from_raw_parts(lut_ptr.offset(e << 1), 2)
-                    )
+                    return wr.write_all(
+                        slice::from_raw_parts(buf_ptr.offset(curr), buf.len() - curr as usize)
+                    );
+
+                // Not easily printable, write down fraction, then full number, then exponent
                 } else {
-                    wr.write_all(&[(e as u8) + b'0'])
-                };
+                    // Single digit, no fraction
+                    if n < 10 {
+                        curr -= 1;
+                        *buf_ptr.offset(curr) = ((n % 10) as u8) + b'0';
+                    } else {
+                        // eagerly decode 4 digits at a time
+                        while n >= 100000 {
+                            let rem = n % 10000;
+                            n /= 10000;
+
+                            let d1 = (rem / 100) << 1;
+                            let d2 = (rem % 100) << 1;
+                            curr -= 4;
+                            ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
+                            ptr::copy_nonoverlapping(lut_ptr.offset(d2), buf_ptr.offset(curr + 2), 2);
+                        }
+
+                        // decode 2 more digits
+                        if n >= 1000 {
+                            let d1 = (n % 100) << 1;
+                            n /= 100;
+                            curr -= 2;
+                            ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
+                        }
+
+                        // decode last 1 or 2 digits
+                        if n < 100 {
+                            curr -= 1;
+                            *buf_ptr.offset(curr) = ((n % 10) as u8) + b'0';
+                            n /= 10;
+                        } else {
+                            let d1 = (n % 100) << 1;
+                            n /= 100;
+                            curr -= 2;
+                            ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
+                        }
+
+                        curr -= 1;
+                        *buf_ptr.offset(curr) = b'.';
+                    }
+                }
             }
+
+            write_num(&mut n, &mut curr, buf_ptr, lut_ptr);
 
             wr.write_all(
                 slice::from_raw_parts(buf_ptr.offset(curr), buf.len() - curr as usize)
@@ -140,3 +159,5 @@ impl Dec64 {
         }
     }
 }
+// }
+//
